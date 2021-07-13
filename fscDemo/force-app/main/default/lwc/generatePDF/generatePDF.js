21229c5cb8cf64d1da1da9a1037d2e05583b8a63
ashusajwan <ashu.sajwan@gmail.com>
2021-07-13 18:16:15 +1000

    fix pdf rendering

--- fscDemo/force-app/main/default/lwc/generatePDF/generatePDF.js
+++ fscDemo/force-app/main/default/lwc/generatePDF/generatePDF.js
@@ -1,12 +1,18 @@
 import { LightningElement } from 'lwc';
-import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import {
+    loadScript,
+    loadStyle
+} from 'lightning/platformResourceLoader';
 import PDFLibSource from '@salesforce/resourceUrl/pdfLib';
 
 export default class GeneratePDF extends LightningElement {
     pdfLibPath;
     pdfLibLoaded = false;
     error;
+    fullUrl = '';
+    // theIframe;
     renderedCallback() {
+        console.log('renderedCallback called');
         if (this.pdfLibLoaded) {
             return;
         }
@@ -14,20 +20,26 @@ export default class GeneratePDF extends LightningElement {
         Promise.all([
             loadScript(this, PDFLibSource)
         ]);
+        console.log('renderedCallback ended');
     }
 
     generatePDF() {
+        console.log('PDF Click called');
         this.createPdf();
     }
 
     async createPdf() {
         try {
+            console.log('PDF generate start');
             const pdfDoc = await PDFLib.PDFDocument.create();
             const page = pdfDoc.addPage([350, 400]);
             page.moveTo(110, 200);
             page.drawText('Hello World!');
             const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
-            this.template.querySelectorAll('pdf').src = pdfDataUri;
+            const theIframe = this.template.querySelector('pdf');
+            this.fullUrl = pdfDataUri;
+            console.log('pdfDataUri' + pdfDataUri);
+            console.log('PDF generate end');
         } catch (error) {
             console.log('error ' + error);
         }
