d11e5c0e847bc4651bd240a45e2eb17301ec5f63
ashusajwan <ashu.sajwan@gmail.com>
2021-07-13 17:16:19 +1000

    update

--- fscDemo/force-app/main/default/lwc/generatePDF/generatePDF.js
+++ fscDemo/force-app/main/default/lwc/generatePDF/generatePDF.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import PDFLibSource from '@salesforce/resourceUrl/pdfLib';
 
-export default class GeneratePDF extends LightningElement {}
+export default class GeneratePDF extends LightningElement {
+    pdfLibPath;
+    pdfLibLoaded = false;
+    error;
+    renderedCallback() {
+        if (this.pdfLibLoaded) {
+            return;
+        }
+        this.pdfLibLoaded = true;
+        Promise.all([
+            loadScript(this, PDFLibSource)
+        ]);
+    }
+
+    generatePDF() {
+        this.createPdf();
+    }
+
+    async createPdf() {
+        try {
+            const pdfDoc = await PDFLib.PDFDocument.create();
+            const page = pdfDoc.addPage([350, 400]);
+            page.moveTo(110, 200);
+            page.drawText('Hello World!');
+            const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+            this.template.querySelectorAll('pdf').src = pdfDataUri;
+        } catch (error) {
+            console.log('error ' + error);
+        }
+    }
+}
